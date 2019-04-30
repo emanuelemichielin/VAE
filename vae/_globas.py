df8e648073cc0c09aa886795e860f1633a1f6ce8
cwfink <cwfink@berkeley.edu>
2019-04-30 00:47:42 +0000

    added paths to globals

--- vae/_globas.py
+++ vae/_globas.py
@@ -1,6 +1,11 @@
 TRACE_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v2/traces/'
 META_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v2/metadata/'
 LABEL_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v2/labels/'
+
+TRACE_PATH_DS = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v3/traces/'
+META_PATH_DS = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v3/metadata/'
+LABEL_PATH_DS = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_v3/labels/'
+
 RQ_DF_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/Run44_processed_DF.pkl'
 EVENT_FILE_MAP_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/file_mapping.h5'
 PARTITION_PATH = '/gpfs/slac/staas/fs1/supercdms/tf/slac/Run44/partitions/'
